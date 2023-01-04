b435d2449e77f9e90b662ac199c59c20fe468250
KiyaNat <kiyaledet@gmail.com>
2023-01-04 00:31:54 -0800

    Task 0 function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,12 +5,11 @@
  * Return: nothing
  */
 void _puts_recursion(char *s)
-
 {
-	if (*s)
+	if (*)
 	{
-		 _putchar(*s);
-		 _puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
