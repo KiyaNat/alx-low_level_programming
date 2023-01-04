91f91dc3f25067e12f6b6a9754c89ab4699b0af6
KiyaNat <kiyaledet@gmail.com>
2023-01-04 00:47:05 -0800

    Task function that returns the length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,1 +1,18 @@
 #include "main.h"
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: string to be measured
+ * Return: Nothing
+ */
+int _strlen_recursion(char *s)
+
+{
+	int leng = 0;
+
+	if (*s)
+	{
+		leng++;
+		leng += _strlen_recursion(s + 1);
+	}
+	return (leng);
+}
